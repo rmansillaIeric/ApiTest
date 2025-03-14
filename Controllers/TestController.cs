695ba8a512b8343358597d5e0c004b4ad7b58b65
rmansillaIeric <mansillar@ieric.org.ar>
2025-03-14 13:04:39 -0300

    [Add] first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class TestController : ControllerBase
+        {
+        // Endpoint GET que devuelve una persona hardcodeada
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Datos hardcodeados para varios objetos TestModel
+            var testData = new List<Testmodel>
+    {
+        new Testmodel
+        {
+            Acta = 1,
+            TipoActa = "I",
+            IERIC = 5222245,
+            Cuit = 20142583692,
+            RazonSocial = "Rodrigo Mansilla",
+            Asignacion = "12/03/2024",
+            Inicio = "",
+            Fin = "",
+            Fiscalizacion = "08/09/2024",
+            Estado = "INICIADO",
+            Girado = "",
+            GiradoFecha = "",
+            Resultado = "Sin multa",
+            Inspector = "Perez",
+            Representacion = "Cordoba"
+        },
+        new Testmodel
+        {
+            Acta = 2,
+            TipoActa = "I",
+            IERIC = 5250,
+            Cuit = 27929584713,
+            RazonSocial = "ANA GARCÍA",
+            Asignacion = "13/03/2024",
+            Inicio = "14/03/2024",
+            Fin = "15/03/2024",
+            Fiscalizacion = "09/09/2024",
+            Estado = "FINALIZADO",
+            Girado = "SÍ",
+            GiradoFecha = "14/03/2024",
+            Resultado = "Multa aplicada",
+            Inspector = "Carlos López",
+            Representacion = "Mendoza"
+        },
+    };
+
+            // Retornar el arreglo como JSON
+            return Ok(testData);
+        }
+
+    }
+
+}
+
---
+++ Testmodel.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1
+{
+    public class Testmodel
+    {
+        public int Acta { get; set; }
+        public string TipoActa { get; set; }
+        public int IERIC { get; set; }
+        public long Cuit { get; set; }
+        public string RazonSocial { get; set; }
+        public string Asignacion { get; set; }
+        public string Inicio { get; set; }
+        public string Fin { get; set; }
+        public string Fiscalizacion { get; set; }
+        public string Estado { get; set; }
+        public string Girado { get; set; }
+        public string GiradoFecha { get; set; }
+        public string Resultado { get; set; }
+        public string Inspector { get; set; }
+        public string Representacion { get; set; }
+    }
+}
