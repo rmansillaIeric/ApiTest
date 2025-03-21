101a6af00c70d69b4e5f918ed89e977256e74c04
rmansillaIeric <mansillar@ieric.org.ar>
2025-03-21 10:17:13 -0300

    [Fix] Query fix in sumarios

--- Controllers/SumarioController.cs
+++ Controllers/SumarioController.cs
@@ -3,6 +3,7 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using Newtonsoft.Json;
 using System.Xml;
+using System.Text.Json.Nodes;
 
 namespace WebApplication1.Controllers
 {
@@ -41,7 +42,7 @@ namespace WebApplication1.Controllers
         [HttpGet("GetSumariosId")]
         public IActionResult GetSumarioId(int idNumeroSumario)
         {
-            string query = "SELECT *,CASE WHEN Estado = 1 THEN 'Iniciado' WHEN Estado = 2 THEN 'Resuleto' WHEN Estado = 3 THEN 'Anulado' WHEN Estado = 4 THEN 'Absuelto' WHEN Estado = 5 THEN 'Habilitado para el pago' WHEN Estado = 6 THEN 'Apelación' WHEN Estado = 7 THEN 'Reconsideración' WHEN Estado = 8 THEN 'Alzada' WHEN Estado = 10 THEN 'Para ejecucion' WHEN Estado = 12 THEN 'En instruccion' WHEN Estado = 13 THEN 'Instruido para resolver' WHEN Estado = 14 THEN 'Recurso en tramite' ELSE CAST(Estado AS VARCHAR) END AS EstadoDescripcion FROM [IERICLegales].[dbo].[Sumarios] WHERE IdNumeroSumario = @IdNumeroSumario";
+            string query = "SELECT *, CASE WHEN Estado = 1 THEN 'Iniciado' WHEN Estado = 2 THEN 'Resuleto' WHEN Estado = 3 THEN 'Anulado' WHEN Estado = 4 THEN 'Absuelto' WHEN Estado = 5 THEN 'Habilitado para el pago' WHEN Estado = 6 THEN 'Apelación' WHEN Estado = 7 THEN 'Reconsideración' WHEN Estado = 8 THEN 'Alzada' WHEN Estado = 10 THEN 'Para ejecución' WHEN Estado = 12 THEN 'En instrucción' WHEN Estado = 13 THEN 'Instruido para resolver' WHEN Estado = 14 THEN 'Recurso en trámite' ELSE CAST(Estado AS VARCHAR) END AS EstadoDescripcion, CASE WHEN FechaPago = CONVERT(DATETIME, '1899-12-30', 120) THEN NULL ELSE FechaPago END AS FechaPagoCorrigida FROM [IERICLegales].[dbo].[Sumarios] WHERE IdNumeroSumario = @IdNumeroSumario";
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
@@ -58,8 +59,9 @@ namespace WebApplication1.Controllers
                         {
                             return NotFound($"No se encontró un sumario con IdNumeroSumario {idNumeroSumario}");
                         }
-                        string jsonResult = JsonConvert.SerializeObject(dataTable, Newtonsoft.Json.Formatting.Indented);
 
+                        string jsonResult = JsonConvert.SerializeObject(dataTable, Newtonsoft.Json.Formatting.Indented);
+                        
                         return Ok(jsonResult);
                     }
                 }
